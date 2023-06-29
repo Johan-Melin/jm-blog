e71bb63a49a52e7b943a318f5dfb4bd7691ac5a2
Johan Melin <soldierheart@hotmail.com>
2023-06-29 13:30:22 +0200

    make PostPreview component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
+import PostPreview from "@/components/PostPreview";
 import getPostMetadata from "@/components/getPostMetadata";
-import Link from "next/link";
 
 export default function Home() {
   const postMetadata = getPostMetadata();
   const postPreviews = postMetadata.map((post) => (
-    <div key={post.slug}>
-      <Link href={`/posts/${post.slug}`}>
-        <h2>{post.title}</h2>
-      </Link>
-      <p>{post.subtitle}</p>
-      <p>{post.date}</p>
-    </div>
+    <PostPreview key={post.slug} {...post} />
   ));
   return <div>{postPreviews}</div>;
 }
