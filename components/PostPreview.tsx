fc523144fcec1a84126b342063c4b69ed4f4f28f
Johan Melin <soldierheart@hotmail.com>
2023-06-29 13:53:47 +0200

    update postPreview css

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -3,12 +3,14 @@ import { PostMetadata } from "./PostMetadata";
 
 const PostPreview = (props: PostMetadata) => {
   return (
-    <div>
+    <div className="p-4 bg-white border rounded-md shadow-md border-violet-200">
       <Link href={`/posts/${props.slug}`}>
-        <h2>{props.title}</h2>
+        <h2 className="font-bold text-violet-600 hover:underline">
+          {props.title}
+        </h2>
       </Link>
-      <p>{props.subtitle}</p>
-      <p>{props.date}</p>
+      <p className="text-sm text-slate-400">{props.date}</p>
+      <p className="text-slate-700">{props.subtitle}</p>
     </div>
   );
 };
