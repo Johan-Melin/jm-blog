0aaabc3de97f3694b2065c9bc7595ccefe00662c
Johan Melin <soldierheart@hotmail.com>
2023-06-29 13:49:57 +0200

    update header + footer css

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,18 +16,18 @@ export default function RootLayout({
 }) {
   const header = (
     <header>
-      <div>
+      <div className="p-8 my-6 text-center rounded-md bg-slate-800">
         <Link href="/">
-          <h1 className="text-3xl font-bold">Hello world!</h1>
+          <h1 className="text-3xl font-bold text-white">Hello world!</h1>
         </Link>
-        <p>Welcome</p>
+        <p className="text-slate-300">Welcome</p>
       </div>
     </header>
   );
 
   const footer = (
     <footer>
-      <div>
+      <div className="py-6 mt-6 text-center border-t border-slate-400 text-slate-400">
         <p>Developed by Johan</p>
       </div>
     </footer>
@@ -36,9 +36,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {header}
-        {children}
-        {footer}
+        <div className="max-w-2xl mx-auto">
+          {header}
+          {children}
+          {footer}
+        </div>
       </body>
     </html>
   );
