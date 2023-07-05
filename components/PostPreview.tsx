5d0dbcded3194fce5819e2602acec7df47d914b4
Johan Melin <soldierheart@hotmail.com>
2023-07-05 17:34:29 +0200

    post list days ago

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -1,11 +1,30 @@
 import Link from "next/link";
 import { PostMetadata } from "./PostMetadata";
 
+const calculateDaysDiff = (dateString: string): number => {
+  const date: Date = new Date(dateString);
+  const currentDate: Date = new Date();
+
+  const daysDiff: number = Math.ceil(
+    Math.abs(currentDate.getTime() - date.getTime()) / (1000 * 3600 * 24)
+  );
+
+  return daysDiff;
+};
+
 const PostPreview = (props: PostMetadata) => {
   return (
     <Link href={`/posts/${props.slug}`}>
       <div className="p-4 bg-white border rounded-md shadow-md border-violet-200">
-        <p className="text-xs text-slate-400">{props.date}</p>
+        <p className="text-xs text-slate-400">
+          {new Date(Date.parse(props.date)).toLocaleDateString("en-US", {
+            //weekday: "long",
+            //year: "numeric",
+            month: "short",
+            day: "numeric",
+          })}
+          , ({calculateDaysDiff(props.date)} days ago)
+        </p>
         <h2 className="text-2xl font-bold hover:text-violet-600">
           {props.title}
         </h2>
