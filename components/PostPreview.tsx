5df7a8058d9436ec5a94e8f5cccad970d17bafc7
Johan Melin <soldierheart@hotmail.com>
2023-07-05 17:19:52 +0200

    make postPreview link

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -3,15 +3,15 @@ import { PostMetadata } from "./PostMetadata";
 
 const PostPreview = (props: PostMetadata) => {
   return (
-    <div className="p-4 bg-white border rounded-md shadow-md border-violet-200">
-      <Link href={`/posts/${props.slug}`}>
-        <h2 className="font-bold text-violet-600 hover:underline">
+    <Link href={`/posts/${props.slug}`}>
+      <div className="p-4 bg-white border rounded-md shadow-md border-violet-200">
+        <p className="text-xs text-slate-400">{props.date}</p>
+        <h2 className="text-2xl font-bold hover:text-violet-600">
           {props.title}
         </h2>
-      </Link>
-      <p className="text-sm text-slate-400">{props.date}</p>
-      <p className="text-slate-700">{props.subtitle}</p>
-    </div>
+        <p className="text-slate-700">{props.subtitle}</p>
+      </div>
+    </Link>
   );
 };
 
