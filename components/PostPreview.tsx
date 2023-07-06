9a5010488d4136ff5404c29781ef502903ae57d4
Johan Melin <soldierheart@hotmail.com>
2023-07-06 13:11:00 +0200

    update date display

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import { PostMetadata } from "./PostMetadata";
 
-const calculateDaysDiff = (dateString: string): number => {
+/*const calculateDaysDiff = (dateString: string): number => {
   const date: Date = new Date(dateString);
   const currentDate: Date = new Date();
 
   return Math.ceil(
     Math.abs(currentDate.getTime() - date.getTime()) / (1000 * 3600 * 24)
   );
-};
+};*/
 
 const PostPreview = (props: PostMetadata) => {
   return (
@@ -17,11 +17,10 @@ const PostPreview = (props: PostMetadata) => {
         <p className="text-xs text-slate-400">
           {new Date(Date.parse(props.date)).toLocaleDateString("en-US", {
             //weekday: "long",
-            //year: "numeric",
+            year: "numeric",
             month: "short",
             day: "numeric",
           })}
-          , ({calculateDaysDiff(props.date)} days ago)
         </p>
         <h2 className="text-2xl font-bold hover:text-violet-600">
           {props.title}
