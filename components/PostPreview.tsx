357e9f47c6f8394f4436065181284c0f9a3446e3
Johan Melin <soldierheart@hotmail.com>
2023-07-11 16:10:05 +0200

    add hover effect preview image

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -13,14 +13,19 @@ import Image from "next/image";
 
 const PostPreview = (props: PostMetadata) => {
   return (
-    <div>
-      <Image
-        src={`/images/${props.image}`}
-        alt=""
-        width="640"
-        height="426"
-        className="w-full"
-      />
+    <Link href={`/posts/${props.slug}`}>
+      <div className="relative overflow-hidden group">
+        <Image
+          src={`/images/${props.image}`}
+          alt=""
+          width="640"
+          height="426"
+          className="block w-full duration-200 ease-in-out group-hover:blur-sm group-hover:scale-105"
+        />
+        <p className="absolute inset-0 p-2 duration-200 ease-in-out translate-x-full bg-white opacity-0 text-dark bg-opacity-40 group-hover:opacity-100 group-hover:translate-x-0">
+          {props.subtitle}
+        </p>
+      </div>
       <div className="p-4 bg-white border rounded-md shadow-md border-light">
         <div className="flex flex-row justify-between">
           <p className="text-xs text-gray">
@@ -33,15 +38,12 @@ const PostPreview = (props: PostMetadata) => {
           </p>
           <p className="text-xs text-gray">{props.time} min read</p>
         </div>
-        <Link href={`/posts/${props.slug}`}>
-          <h2 className="text-2xl font-bold hover:text-link">{props.title}</h2>
-        </Link>
-        <p className="text-dark">{props.subtitle}</p>
+        <h2 className="text-2xl font-bold hover:text-link">{props.title}</h2>
         {props.tags.map((tag) => (
           <span className="p-1 rounded-md hover:bg-light">#{tag} </span>
         ))}
       </div>
-    </div>
+    </Link>
   );
 };
 
