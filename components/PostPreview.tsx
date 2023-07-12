3b3ceff9fd9a2bc239b2543a27c385659d1e7a8e
Johan Melin <soldierheart@hotmail.com>
2023-07-12 14:55:50 +0200

    add lazy loading images

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -17,16 +17,14 @@ const PostPreview = (props: PostMetadata) => {
   return (
     <Link href={`/posts/${props.slug}`}>
       <div className="relative overflow-hidden group">
-        <div className="blur-load">
-          <Image
-            src={`/images/${props.image}`}
-            alt=""
-            width="640"
-            height="426"
-            loading="lazy"
-            className="block w-full duration-200 ease-in-out group-hover:blur-sm group-hover:scale-105"
-          />
-        </div>
+        <Image
+          src={`/images/${props.image}`}
+          alt=""
+          width="640"
+          height="426"
+          loading="lazy"
+          className="block w-full duration-200 ease-in-out group-hover:blur-sm group-hover:scale-105"
+        />
         <p className="absolute inset-0 p-2 duration-200 ease-in-out translate-x-full bg-white opacity-0 text-dark bg-opacity-40 group-hover:opacity-100 group-hover:translate-x-0">
           {props.subtitle}
         </p>
