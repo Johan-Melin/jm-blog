74a8980171950f320dcfd65e669015de562dd7f2
Johan Melin <soldierheart@hotmail.com>
2023-07-13 17:19:20 +0200

    change image aspect ratio

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -16,16 +16,15 @@ const PostPreview = (props: PostMetadata) => {
 
   return (
     <Link href={`/posts/${props.slug}`} className="group">
-      <div className="relative overflow-hidden">
+      <div className="relative h-48 overflow-hidden">
         <Image
           src={`/images/${props.image}`}
           alt=""
-          width="640"
-          height="426"
           loading="lazy"
           className="block w-full duration-200 ease-in-out group-hover:blur-sm group-hover:scale-105 bg-gray"
           placeholder="blur"
-          blurDataURL={`${props.imageBlurData}`}
+          blurDataURL={props.imageBlurData}
+          fill
         />
         <p className="absolute inset-0 flex items-center p-2 text-center text-white duration-200 ease-in-out translate-y-full opacity-0 bg-dark bg-opacity-40 group-hover:opacity-100 group-hover:translate-y-0">
           {props.subtitle}
