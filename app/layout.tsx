a190b8214b7998f314d48757f981a09c9b22ea42
Johan Melin <soldierheart@hotmail.com>
2025-02-17 18:18:31 +0100

    removing sidebar, changing background

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,8 @@ export default function RootLayout({
     <header>
       <div className="px-6 py-4">
         <Link href="/">
-          <h1 className="inline p-1 text-2xl font-bold bg-white rounded-md">
-            PIC
+          <h1 className="inline p-1 text-2xl font-bold">
+            Dev Narratives
           </h1>
         </Link>
       </div>
@@ -38,41 +38,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="flex flex-col min-h-screen bg-dark">
-          <div className="fixed w-full h-full z-10">
-            <Image
-              src="/background.jpg"
-              alt="Background Image"
-              layout="fill"
-              objectFit="cover"
-              quality={75}
-              priority={false} 
-              sizes="(max-width: 640px) 640px, (max-width: 768px) 768px, (max-width: 1024px) 1024px, 1280px"
-              className="z-10 fixed hidden md:block"
-            />
-            <div className="absolute inset-0 z-20 hidden md:block" style={{background: 'radial-gradient(circle, transparent, rgba(0, 0, 0, 0.75))'}}></div>
-          </div>
+        <div className="flex flex-col min-h-screen">
           <header className="z-30">
             {header}
           </header>
           <div className="flex flex-row">
-            <nav className="w-64 bg-gray-800 text-white p-6 z-30">
-              <ul>
-                <li className="sidebar-item">
-                  <Link className="sidebar-link" href="/">
-                    <div style={{backgroundColor: "cyan"}}><FaPencilAlt /></div>
-                    Blog
-                  </Link>
-                </li>
-                <li className="sidebar-item">
-                  <Link className="sidebar-link" href="/chess/vision/">
-                  <div style={{backgroundColor: "green"}}><SiChessdotcom /></div>
-                    Chess Vision
-                  </Link>
-                </li>
-                {/* Add more menu items here */}
-              </ul>
-            </nav>
             <div className="flex-grow max-w-5xl p-6 mx-auto z-30">
               {children}
             </div>
