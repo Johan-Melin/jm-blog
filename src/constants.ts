f5f4d397d829477babe9706cc4277a149e504baa
Johan Melin <soldierheart@hotmail.com>
2026-02-21 07:59:26 +0100

    chore: update socials

--- src/constants.ts
+++ src/constants.ts
@@ -19,19 +19,19 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/Johan-Melin",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
     name: "X",
-    href: "https://x.com/username",
+    href: "https://x.com/thegamedev",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/johan-melin",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
